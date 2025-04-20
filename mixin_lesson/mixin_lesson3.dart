a05eb9b8246d535e3ac06a1f4ce372ce7f20e137
Ly Nguyen <ly@ubuntu-machine.com>
2025-04-19 17:35:27 -0700

    Update mixin

--- mixin_lesson/mixin_lesson3.dart
+++ mixin_lesson/mixin_lesson3.dart
@@ -1,7 +1,19 @@
+/**
+ * Mixin allows you to reuse feature of another class without inheritance. It helps a class to reuse features from multiples entities
+ * It is used to solve the issue of single inheritance
+ * 
+ */
+
 mixin Hashable {
+  // we need to define the type of property1 and property2 so any class extending this mixin can reuse the property1 and property2
+  int? property1; // we need to define a
+  String? property2;
+
   @override
   int get hashCode {
     // Calculate the hash code based on the object's properties
+    // in this example, the object has property1 and property2
+    // we calculate the total hashcode of it based on property1 and property2 hashcode
     int hash = 17;
     hash = 31 * hash + property1.hashCode;
     hash = 31 * hash + property2.hashCode;
@@ -10,6 +22,11 @@ mixin Hashable {
 }
 
 /// A class representing an example object.
+/// hashCode is a special property used to support efficient lookup and comparison of objects,
+/// especially in hash-based collections like Set and Map.
+/// hashCode is an integer representation of an object
+/// it must be equivalent with ==; if A == B is true, then A.hashCode == B.hashCode is also true
+/// When we look up an object in Set or Map, we can compute its hash and see if the hash exists in the set or map
 class MyClass with Hashable {
   final int property1;
   final String property2;
